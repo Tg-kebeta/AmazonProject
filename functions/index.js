df2154990b4e724404ca2b0c4304239513667de5
Tg-Kebeta <tigist.chefessa@gmail.com>
2025-10-19 20:24:46 -0400

    updated css for payment and orders

--- functions/index.js
+++ functions/index.js
@@ -50,30 +50,5 @@ exports.api = onRequest(app);
 
 
 
-// dotenv.config();
-// const stripe = new Stripe(process.env.STRIPE_KEY);
 
-// const app = express();
-// app.use(cors({ origin: true }));
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.status(200).json({ message: "Success !" });
-// });
-
-// app.post("/payment/create", async (req, res) => {
-//   const total = parseInt(req.query.total);
-//   if (total > 0) {
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount: total,
-//       currency: "usd",
-//     });
-//     res.set("Access-Control-Allow-Origin", "*");
-//     res.status(201).json({ clientSecret: paymentIntent.client_secret });
-//   } else {
-//     res.status(403).json({ message: "total must be greater than 0" });
-//   }
-// });
-
-// export const api = onRequest(app);
 
